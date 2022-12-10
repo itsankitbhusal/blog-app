4644e69c4405b7f0b3b2a9837b16d263f1fee0ca
ankitbhusal <ankitbhusal95@gmail.com>
2022-12-10 23:18:55 +0545

    blog section

--- frontend/src/components/Hero/BlogCard.jsx
+++ frontend/src/components/Hero/BlogCard.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 
-//   id,
-//   image,
-//   title,
-//   userId,
-//   body,
-//   categoryId,
-//   createdAt,
-//   UpdatedAt,
+/* 
+  id
+  body
+  title
+  image
+  category
+    id
+    name
+  user
+  id
+   firstName
+   lastName
+
+
+
+*/
 
 const BlogCard = ({ blog }) => {
   return (
@@ -16,12 +24,16 @@ const BlogCard = ({ blog }) => {
         ? blog.map((post, index) => (
             <div key={index}>
               <img src={post.image} alt="" />
+              <span>{post.category.name}</span>
               <h1>{post.title}</h1>
-              <p>{post.body}</p>
-              <p>{post.userId}</p>
-              <p>{post.categoryId}</p>
-              <p>{post.createdAt}</p>
-              <p>{post.UpdatedAt}</p>
+              {/* limit body to preview characters  */}
+              <p>{post.body.slice(0, 200) + "..."}</p>
+              <p>{post.user.firstName + " " + post.user.lastName}</p>
+              <span>
+                {new Date(post.createdAt).toDateString().slice(4, 10) +
+                  ", " +
+                  new Date(post.createdAt).toTimeString().slice(0, 5)}
+              </span>
             </div>
           ))
         : null}
